48de7f4abf91d4b56aa7b593587deb015207792e
LadderLay <2509127422@qq.com>
2018-12-13 23:04:18 +0800

    fix#2

--- task-50 project/16.c
+++ task-50 project/16.c
@@ -1,12 +1,36 @@
 #include<stdio.h>
 
+void str_dcount(const char s[]){
+  int cnt[110] = {0};
+  char t[] = "aeiou";
+  int i = 0;
+  int x = 0;
+  while(s[i]){
+  	for (x = 0;x < 5;x++){
+  	  if (s[i] == t[x])
+  	    cnt[x]++;
+     }
+     i++;
+  }
+  for(i = 0;i < 5; i++)
+  printf("%c£º%d\n",t[i], cnt[i]);
+}
+
 int main(void){
-    int a, i;
-    int b[100];
-    scanf("%d", &a);
-        for(i = 0;i < a; i++)
-        scanf("%d",&b[i]);
-        for(i = 0;i < a; i++)
-        printf("%c",b[i]);
+  int a, b, i, t;
+  char str[100][110], s[110];
+  while(scanf("%d", &a) != EOF){
+  	for(i = 0;i < a; i++)
+  	  scanf("%s", str[i]);
+  	  for(t = 0;t < a;t++){
+  	    while(str[t][i]){
+  	      s[i] = str[t][i];
+   	      i++;
+        }
+        str_dcount(s);
+   	      if(t < a - 1 )
+   	      printf("\n");
+      }
+   }
 return 0;
 }
